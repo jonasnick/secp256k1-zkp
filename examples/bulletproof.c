e3971a500e732e1dbc676b8bb7d5d01d27784cc1
Jonas Nick <jonasd.nick@gmail.com>
2020-01-17 13:48:05 +0000

    Switch bulletproof example to purify

--- examples/bulletproof.c
+++ examples/bulletproof.c
@@ -1,10 +1,10 @@
 #include <assert.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include <secp256k1.h>
 #include <secp256k1_bulletproofs.h>
-#include "src/scalar.h"
 
 void printn(unsigned char *s, size_t n) {
     size_t i;
@@ -55,46 +55,73 @@ int main(void) {
     secp256k1_bulletproof_circuit_assignment *assn;
     const unsigned char blind[32] = "   i am not a blinding factor   ";
     const unsigned char *blinds[1];
+    const unsigned char value[32] = { 0x28, 0x38, 0x7b, 0xf1, 0x14, 0x18, 0xef, 0x01, 0x85, 0xb7, 0xa7, 0xd7, 0x3f, 0xd2, 0x85, 0xcf, 0x5d, 0x10, 0x27, 0x5b, 0xdb, 0xb5, 0x63, 0x9f, 0x16, 0x02, 0x40, 0xa7, 0x30, 0x52, 0xaa, 0xa5 };
     unsigned char nonce[32] = "my kingdom for some randomness!!";
     secp256k1_pedersen_commitment commitment;
     /* This means 2 gates, 1 commitment (with Vi), 0 bits, 5 constraints */
-    const char inv_17_19_circ[] = "2,1,0,5; L0 = 17; 2*L1 - L0 = 21; O0 = 1; O1 = 1; V0 - L0 = 100;";
+    FILE *fcirc = fopen("./src/modules/bulletproofs/bin_circuits/purify.circ", "r");
+    char *inv_17_19_circ;
+    secp256k1_bulletproof_circuit *simple;
+    secp256k1_generator blinding_gen = secp256k1_generator_const_h;
+    secp256k1_generator value_gen = secp256k1_generator_const_g;
+    {
+        long fsize;
+        if (fcirc == NULL) {
+            printf("ERR read circuit\n");
+            return 0;
+        }
+        fseek(fcirc, 0, SEEK_END);
+        fsize = ftell(fcirc);
+        fseek(fcirc, 0, SEEK_SET);  /* same as rewind(f); */
+        inv_17_19_circ = malloc(fsize + 1);
+        fread(inv_17_19_circ, 1, fsize, fcirc);
+        fclose(fcirc);
+        inv_17_19_circ[fsize] = 0;
+    }
+
     /* So implied is that L0*R0 = O0 and L1*R1 = O1 */
-    secp256k1_bulletproof_circuit *simple = secp256k1_bulletproof_circuit_parse(ctx, inv_17_19_circ);
-
-    blinds[0] = blind;
-
-    scratch = secp256k1_scratch_space_create(ctx, 100000);
-
+    simple = secp256k1_bulletproof_circuit_parse(ctx, inv_17_19_circ);
     if (simple == NULL) {
         printf("ERR parse\n");
         return 0;
     }
-    gens = secp256k1_bulletproof_generators_create(ctx, &secp256k1_generator_const_h, 256, 1);
+
+    blinds[0] = blind;
+    scratch = secp256k1_scratch_space_create(ctx, 1000000);
+    gens = secp256k1_bulletproof_generators_create(ctx, &blinding_gen, 2*2048, 1);
     if (gens == NULL) {
         printf("ERR create\n");
         return 0;
     }
 
     /* Everything in assn is private, the values `v` must end up in the "commitments" */
-    assn = secp256k1_bulletproof_circuit_assignment_decode(ctx, "foo.assn");
+    assn = secp256k1_bulletproof_circuit_assignment_decode(ctx, "./src/modules/bulletproofs/bin_circuits/purify.assn");
     if (assn == NULL) {
         printf("ERR decoding\n");
         return 0;
     }
 
+    if (!secp256k1_bulletproof_circuit_evaluate(simple, assn, value)) {
+      printf("ERR eval\n");
+      return 0;
+    }
+
     /* This should prove that pedersen commitment v*H + r*G includes a v that satisfies the constraints */
-    if (!secp256k1_bulletproof_circuit_prove(ctx, scratch, gens, simple, proof, &plen, assn, blinds, 1, nonce, &secp256k1_generator_const_h, NULL, 0)) {
+    if (!secp256k1_bulletproof_circuit_prove(ctx, scratch, gens, simple, proof, &plen, assn, blinds, 1, nonce, &value_gen, NULL, 0)) {
         printf("ERR proving\n");
         return 0;
     }
-
-    if (!secp256k1_pedersen_commit(ctx, &commitment, blinds[0], 117, &secp256k1_generator_const_h, &secp256k1_generator_const_h)) {
+   
+    printf("plen = %ld\n", plen);
+    printf("verifying\n");
+    
+    printf("committing\n");
+    if (!secp256k1_pedersen_commit_char(ctx, &commitment, blinds[0], value, &value_gen, &blinding_gen)) {
         printf("ERR commit\n");
         return 0;
     }
 
-    if (!secp256k1_bulletproof_circuit_verify(ctx, scratch, gens, simple, proof, plen, &commitment, 1, &secp256k1_generator_const_h, NULL, 0)) {
+    if (!secp256k1_bulletproof_circuit_verify(ctx, scratch, gens, simple, proof, plen, &commitment, 1, &value_gen, NULL, 0)) {
         printf("ERR verifying\n");
         return 0;
     }
